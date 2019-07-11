104dbd0a8d5a8ed94d6c3cd91649cc97c0c3aeac
Nathan DuPont <nathan.dupont01@gmail.com>
2019-07-10 22:43:12 -0500

    Initial Commit
    All files currently on my personal website. Initial Commit for first transfer to GitHub

---
+++ site/script.js
@@ -0,0 +1,6 @@
+window.onload = function() { //Custom scaling for header and content
+    var menuItems = document.getElementById("navbar").getElementsByTagName("li"); // Get number of tabs present 
+    for (let tab of menuItems) {
+        console.log("test");
+    }
+};
